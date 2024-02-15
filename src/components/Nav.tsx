ce55c841b1973d3ddb2c3207f2d15a61e6ab9816
Mark <muchirimark2@gmail.com>
2024-02-16 02:04:50 +0300

    Added 1st home section

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,15 +2,19 @@ function Nav() {
   return (
     <>
       <div
-        className="bg-white w-full h-20 flex justify-between flex-row items-center px-24 border-b-4"
+        className="bg-white w-full h-20 flex justify-between flex-row items-center px-36"
       >
         <div className="py-0.5">
-          <p className="text-4xl font-extrabold items-center">Logo</p>
+          <p className="text-4xl font-extrabold items-center">
+            Logo
+          </p>
         </div>
         <div className="flex flex-row">
           <div>
             <ul className="relative flex justify-end gap-14 pr-10 items-center top-4 text-xl">
-              <li className="font-semibold text-amber-800">Home</li>
+              <li className="font-semibold text-amber-800">
+                Home
+              </li>
               <li>Shop</li>
               <li>About us</li>
               <li>Contact</li>
