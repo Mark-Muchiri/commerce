9a9fc04d7dd7c855544d312ec159d188f4075303
Mark <muchirimark2@gmail.com>
2024-03-08 00:43:39 +0300

    corrected the Nav error

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import "react-lazy-load-image-component/src/effects/blur.css";
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Nav() {
   return (
