978858f2a8e9acac6b3e6884e59a559fef177b96
Mark <muchirimark2@gmail.com>
2024-03-15 03:16:06 +0300

    contact us form styling done

--- src/pages/contact/Section1.tsx
+++ src/pages/contact/Section1.tsx
@@ -4,8 +4,8 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 function Section1() {
 	return (
 		<section className=''>
-			<div className='flex justify-between items-start gap-24'>
-				<div className=''>
+			<div className='flex justify-between items-start'>
+				<div className='w-full'>
 					{/* details */}
 					<div className='w-12/12'>
 						{/* header */}
@@ -20,29 +20,62 @@ function Section1() {
 							</div>
 						</div>
 
-						{/* paragraph */}
-						<div className=' '>
-							<p className='relative pt-7 text-[1.4rem] leading-8 text-neutral-800'>
-								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non
-								veritatis alias odio similique facere dolores, a consectetur
-								perferendis voluptatibus asperiores aut corporis pariatur quis
-								nemo earum nulla quisquam voluptas nostrum?Lorem ipsum dolor
-								sit, amet consectetur adipisicing elit. Non veritatis alias odio
-								similique facere dolores
-							</p>
-						</div>
+						{/* Form */}
+						<form className='pt-3'>
+							{/* Name input */}
+							<div className='pt-1'>
+								<p className="relative pl-2 text-[1.3rem] leading-8 text-neutral-500 pb-1">
+									Name
+								</p>
+								<input
+									type='text'
+									// placeholder='Jon Doe'
+									className='flex-grow px-7 py-3 w-full text-2xl border border-solid border-gray-400 rounded-xl'
+								/>
+							</div>
+							{/* email input */}
+							<div className='pt-6'>
+								<p className="relative pl-2 text-[1.3rem] leading-8 text-neutral-500 pb-1">
+									Email
+								</p>
+								<input
+									type='email'
+									// placeholder='Jon Doe'
+									className='flex-grow px-7 py-3 w-full text-2xl border border-solid border-gray-400 rounded-xl'
+								/>
+							</div>
+							{/* message input */}
+							<div className='pt-6'>
+								<p className="relative pl-2 text-[1.3rem] leading-8 text-neutral-500 pb-1">
+									Message
+								</p>
+								<textarea
+									placeholder='Type your message here'
+									className='flex-grow px-7 py-3 w-full h-40 text-2xl border border-solid border-gray-400 rounded-xl resize-y overflow-auto'
+								/>
+							</div>
+							{/* checkbox input */}
+							<div className='pt-6'>
+								<label className="inline-flex items-center cursor-pointer">
+									<input type="checkbox" name="option1" value="Option 1" className="sr-only peer" />
+									<div className="w-6 h-6 bg-white border-2 border-gray-400 peer-checked:border-blue-500 rounded-md peer-focus:ring-4 peer-focus:ring-blue-200 peer-checked:bg-blue-500"></div>
+									<span className="ml-2 text-gray-600 text-lg pl-1">I accept the terms</span>
+								</label>
+							</div>
+
+						</form>
 					</div>
 				</div>
 
 				{/* image */}
-				<div className='px-20'>
+				<div className='pl-20'>
 					<LazyLoadImage
-						className='relative w-[400px] h-[550px] rounded-[20px] object-cover drop-shadow-xl'
+						className='relative w-[450px] h-[620px] rounded-[20px] object-cover drop-shadow-xl'
 						effect='blur'
 						src={`https://images.unsplash.com/photo-1599004831521-f9b1f81f5a84?q=80&w=1438&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`}
 						alt={``}
-						height={`550px`}
-						width={`400px`}
+						height={`620px`}
+						width={`450px`}
 					/>
 				</div>
 			</div>
