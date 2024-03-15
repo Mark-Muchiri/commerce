d3d5f07bbefb7e272f9c9a5d62046c5f45c776e6
Mark <muchirimark2@gmail.com>
2024-03-15 18:46:08 +0300

    changes

--- src/pages/contact/Section2.tsx
+++ src/pages/contact/Section2.tsx
@@ -1,8 +1,13 @@
 function Section2() {
   return (
     <section>
+      <div className="flex justify-center items-center">
+        <p className="text-center text-5xl my-16 font-mono font-semibold text-gray-800">
+          This is the Icon section
+        </p>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Section2
+export default Section2;
